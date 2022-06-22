62306d1d5fbb7921cfe4b47177f52c95afd3f209
s-goto-towa <107522190+s-goto-towa@users.noreply.github.com>
2022-06-22 13:04:22 +0900

    Add files via upload

---
+++ servlet/LogoutServlet.java
@@ -0,0 +1,25 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LogoutServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// セッション情報を取得
+		HttpSession session = request.getSession();
+		// セッションがある場合、セッションを破棄
+		if (session != null) {
+			session.invalidate();
+		}
+		// login.jspにフォワード
+		request.getRequestDispatcher("/view/login.jsp").forward(request, response);
+	}
+
+}
+
